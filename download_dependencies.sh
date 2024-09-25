950139b4edef71a4360e8d72548cf4974e4b6492
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-25 17:44:52 -0400

    beta

---
+++ download_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+
+curl --output "$PWD/js/qrcode.min.js" "https://unpkg.com/qrcodejs@1.0.0/qrcode.min.js"
+
+ curl --output "$PWD/js/easy-file-saver.js" "https://unpkg.com/easy-file-saver@0.2.0/easy-file-saver.js"
+
+ 
